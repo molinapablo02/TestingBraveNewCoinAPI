7f2e8cc0a7f15f8effb47b54aacb5faf4a206758
molinapablo02 <molinapablo02@gmail.com>
2022-11-28 13:34:04 -0300

    New changes are added in Base and Asset

--- src/test/java/Asset.java
+++ src/test/java/Asset.java
@@ -8,30 +8,47 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Asset extends Base {
+
     private static RequestSpecification request;
     private static Response response;
 
     // primo obterer un asset para despues hacer un assetTicker si no me equivoco
     @Test
     public void getAsset() {
-        // assetId ? = un ejemplo f1ff77b6-3ab4-4719-9ded-2fc7e71cff1f
-        // Setting up the request and response.
-        request = given()
+        /*
+         * // assetId ? = un ejemplo f1ff77b6-3ab4-4719-9ded-2fc7e71cff1f
+         * // Setting up the request and response.
+         * request = given()
+         * .header("X-RapidAPI-Key",
+         * "a1a2b4eb17msh6a7fed06a8297d5p16dae6jsna5e67b4f306c")
+         * .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
+         * .baseUri("https://bravenewcoin.p.rapidapi.com")
+         * .basePath("/asset?status=ACTIVE")
+         * .log().all();
+         * 
+         * // preguntar obtener colecciones donde "type" sea "FIAT" y status sea
+         * "ACTIVO"
+         * // rta: agregar "&type=FIAT" al path
+         * response = request
+         * .when()
+         * .get("/asset?status=ACTIVE&type=FIAT")
+         * .prettyPeek();
+         * 
+         */
+
+        // A request to the API.
+        response = given()
                 .header("X-RapidAPI-Key", "a1a2b4eb17msh6a7fed06a8297d5p16dae6jsna5e67b4f306c")
                 .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
-                .baseUri("https://bravenewcoin.p.rapidapi.com")
-                .basePath("/asset?status=ACTIVE")
-                .log().all();
-
-        response = request
                 .when()
-                .get("/asset?status=ACTIVE")
+                .get("/asset?status=ACTIVE&type=FIAT")
                 .prettyPeek();
 
-        // obtener colecciones cuyo "type" sea "FIAT"
+        // mas o menos bien...
 
-        ArrayList<String> typeList = response.path("content.type");
-        System.out.println(typeList);
+        ArrayList<String> foreingCurrency = response.jsonPath().get("content.name");
+
+        Assert.assertTrue(foreingCurrency.size() > 10);
 
     }
 }
--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,3 +1,4 @@
+
 import org.testng.annotations.BeforeClass;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -6,6 +7,10 @@ import io.restassured.specification.RequestSpecification;
 
 public class Base {
 
+  // Me parece que hacer esto es la mejor opcion cuando tengo un endpoint en comun
+  // para hacerlo
+  // mas rapido. el endpoint en comun aca es "api"
+  // por ejemplo: https://bravenewcoin.p.rapidapi.com/api/...
   @BeforeClass
   public static void setUpRestAsurred() {
 
@@ -26,7 +31,9 @@ public class Base {
 /*
  * Probar la clase Base solo con la baseURI, agregar la basePath en la clase que
  * sea manualmente.
- * 
+ * rta:funciona y me parece mas adecuado
+ * ------------
  * Probar la clase Base sin baseURI y basePath, agregarla en la clase que sea
  * necesaria manualmente.
+ * rta: tambien se puede
  */
